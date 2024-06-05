34da2eef909959c05b9b41568d21852839494b43
Deysox <deysox@laposte.net>
2024-06-05 16:39:42 +0200

    final

--- src/tutorial_popup.gd
+++ src/tutorial_popup.gd
@@ -1,12 +1,12 @@
 extends Control
 
 var texts:Dictionary = {
-	"intro": "Et me revoila dans ces souterrins de malheur...Enfer, il va bien falloir que je sorte d'ici 
-	\n Mon vieux couteau me sera peut-être utile. C'est comment déja... <E> pour donner un coup ?",
-	"mob": "Il va me falloir etre rapide pour échapper aux monstres. Bondir avec <Espace> peut etre une sollution...",
-	"spell": "Un sort ! Les flèches dirrectionnelles me permettrons de m'en servir",
+	"intro": "Et me revoila dans ces souterrains de malheur... Enfer, il va bien falloir que je sorte d'ici ! 
+	\n Mon vieux couteau me sera sans doute utile. C'est comment déja... <E> pour donner un coup ?",
+	"mob": "Il va me falloir courir vite pour échapper aux monstres. Bondir avec <Espace> peut être une sollution...",
+	"spell": "Un sort ! Les flèches directionnelles me permettront de m'en servir.",
 	"spell_use": "L'essence me permet de lancer des sorts, mais également de me protéger. Serait-il plus prudent de la conserver ?",
-	"lowHealth": "Le couteau et le bond me vident mon esprit... Avec les monstres a gérer, je vai devoir etre prudente !"}
+	"lowHealth": "Le couteau et le bond vident mon esprit... Avec les monstres à gérer, je vais devoir rester attentive !"}
 
 func change_text(new_text_type:String):
 	$tutorial_text.visible = true
